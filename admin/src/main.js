5229403fd767fd3ca82b3b06d083193579d62f65
wishong <hjq690264423@vip.qq.com>
2019-12-18 17:00:07 +0800

    finish 12.18

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import http from './network/http'
 import './plugins/element.js'
 
 // 富文本本编辑器
@@ -14,6 +15,8 @@ import VueParticles from 'vue-particles'
 Vue.use(VueParticles)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = http;
 
 new Vue({
   router,
--- client/src/views/about/comps/AboutMe.vue
+++ client/src/views/about/comps/AboutMe.vue
@@ -3,8 +3,9 @@
     <sub-copm>
       <h2 slot="title">关于我</h2>
       <div slot="content" class="about-me">
+        <p>HUT 2020物联网工程本科毕业生。</p>
         <p>为什么选择做前端？机缘巧合，兴趣使然。</p>
-        <p>目前掌握的技能为html、css、javascirpt、jqurery、vue以及小部分express和mongodb。</p>
+        <p>目前熟悉掌握Vue框架以及部分express和mongodb。</p>
         <p>往后会探索和学习新的技术栈和前沿技术。</p>
       </div>
     </sub-copm>
